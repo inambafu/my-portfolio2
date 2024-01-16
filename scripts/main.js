a8d7fe8d9985cbe5dc4ec15fc54a47eeb8954ec3
Isaac B Nambafu <buzack4@gmail.com>
2024-01-17 02:03:28 +0300

    more js
    a sedond Typed was added

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,14 @@ $(document).ready(function(){
 		backSpeed: 60,
 		loop:true
 	});
+
+	//typing effect 2
+	var typed = new Typed(".typing-2", {
+		strings: ["Web Designer", "Web Developer", "Freelancer", "Consultant",],
+		typeSpeed: 100,
+		backSpeed: 70,
+		loop:true
+	});
 	
 });
 
