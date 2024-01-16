fa4b87d10ca9e2f5a158254a8baa7f7539a25848
Isaac B Nambafu <buzack4@gmail.com>
2024-01-16 22:44:12 +0300

    Js
    the javascript for a sticky navbar and Typed written

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		//fixed navbar on scroll
+
+		if(this.scrolly > 20) {
+			$('.navbar').addClass("sticky");
+		}else{
+			$('.navbar').removeClass("sticky");
+	});
+
+	//typing effect
+
+	var typed = new Typed(".typing", {
+	    strings: ["Web Designer", "Web Developer", "Freelancer", "Consultant"],
+	    typeSpeed: 100,
+            backSpeed: 60,
+            loop:true
+	});
+});
